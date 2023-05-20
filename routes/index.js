0ca688b3776d099677a7bf34860befa29c8a220b
jadenday20 <day18007@byui.edu>
2023-05-20 12:29:49 -0600

    added routes.rest

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const routes = express.Router();
 
-routes.use("/songs", require("./songs"));
-routes.use("/students", require("./students"));
+routes
+  .use("/songs", require("./songs"))
+  .use("/students", require("./students"));
 
 module.exports = routes;
