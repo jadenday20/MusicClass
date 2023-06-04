808a3909d59a1a218d5b1512adfe1aa89c6baece
jadenday20 <day18007@byui.edu>
2023-06-03 22:34:26 -0600

    added authorization which doesn't work properly yet

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const routes = express.Router();
+var session = require("express-session");
+var passport = require("passport");
+
+var SQLiteStore = require("connect-sqlite3")(session);
+routes.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    // store: new SQLiteStore({ db: "sessions.db", dir: "./var/db" }),
+    store: new SQLiteStore(),
+  })
+);
+routes.use(passport.authenticate("session"));
 
 routes
   .use("/songs", require("./songs"))
-  .use("/students", require("./students"));
+  .use("/students", require("./students"))
+  .use("/auth", require("./auth"));
 
 module.exports = routes;
